c0a64817f736ebb61ee17d1a473f3fc8037813d5
LazarevSlava <549132w@gmail.com>
2024-01-22 15:13:44 +0200

    search created

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,32 +1,19 @@
 import { useState } from 'react';
 import style from '../components/Search.module.scss';
+import { useDebounce } from '../hooks/useDebounce';
 
 function Search({ onSearch = Function.prototype }) {
     const [value, setValue] = useState('');
 
-    const debouncedSendRequest = debounce(onSearch, 500);
+    const handleSearch = useDebounce((value) => {
+        onSearch(value);
+    }, 350);
 
     const onChange = (e) => {
         const updatedValue = e.target.value;
         setValue(updatedValue);
-        debouncedSendRequest();
+        handleSearch(updatedValue);
     };
-
-    // useEffect(() => {
-    //     // Skip the initial invocation on mount
-    //     if (isMounted.current) {
-    //         timeoutIdRef.current = setTimeout(() => {
-    //             onSearch(value);
-    //         }, 350);
-
-    //         return () => {
-    //             clearTimeout(timeoutIdRef.current);
-    //         };
-    //     } else {
-    //         // Set isMounted to true after the initial render
-    //         isMounted.current = true;
-    //     }
-    // }, [value, onSearch]);
 
     return (
         <div className={style.input_field}>
@@ -34,7 +21,6 @@ function Search({ onSearch = Function.prototype }) {
                 type="search"
                 id={style.search}
                 placeholder="Search for a country..."
-                onKeyDown={handleKey}
                 onChange={onChange}
                 value={value}
             />
---
+++ src/hooks/useDebounce.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+
+export const useDebounce = (callback, delay) => {
+    const timeoutRef = useRef(null);
+
+    useEffect(() => {
+        // Cleanup the previous timeout on re-render
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
+
+    const debouncedCallback = (...args) => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(() => {
+            callback(...args);
+        }, delay);
+    };
+
+    return debouncedCallback;
+};
