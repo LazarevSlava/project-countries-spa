c3c2f24a316bc0bf68e0ffb052b0fd45bba6a4fa
LazarevSlava <549132w@gmail.com>
2024-01-23 08:44:03 +0200

    lint fixed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,38 +1,38 @@
-import { useState } from 'react';
-import style from '../components/Search.module.scss';
-import { useDebounce } from '../hooks/useDebounce';
-import PropTypes from 'prop-types';
-import { useTheme } from '../hooks/themeUtils';
+import { useState } from "react";
+import style from "../components/Search.module.scss";
+import { useDebounce } from "../hooks/useDebounce";
+import PropTypes from "prop-types";
+import { useTheme } from "../hooks/themeUtils";
 function Search({ onSearch = Function.prototype }) {
-    const [value, setValue] = useState('');
-    const { theme = 'dark' } = useTheme();
-    const searchStyle = `${style.search} ${theme === 'dark' ? ` ${style.light}` : `''`}`;
+  const [value, setValue] = useState("");
+  const { theme = "dark" } = useTheme();
+  const searchStyle = `${style.search} ${theme === "dark" ? ` ${style.light}` : `''`}`;
 
-    const handleSearch = useDebounce((value) => {
-        onSearch(value);
-    }, 350);
+  const handleSearch = useDebounce((value) => {
+    onSearch(value);
+  }, 350);
 
-    const onChange = (e) => {
-        const updatedValue = e.target.value;
-        setValue(updatedValue);
-        handleSearch(updatedValue);
-    };
+  const onChange = (e) => {
+    const updatedValue = e.target.value;
+    setValue(updatedValue);
+    handleSearch(updatedValue);
+  };
 
-    return (
-        <div className={style.input_field}>
-            <input
-                className={searchStyle}
-                type="search"
-                placeholder="Search for a country..."
-                onChange={onChange}
-                value={value}
-            />
-        </div>
-    );
+  return (
+    <div className={style.input_field}>
+      <input
+        className={searchStyle}
+        type="search"
+        placeholder="Search for a country..."
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
 }
 
 Search.propTypes = {
-    onSearch: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
 };
 
 export { Search };
