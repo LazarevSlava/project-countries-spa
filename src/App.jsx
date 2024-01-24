3d013a545c0e7281dbbcf7ac48040a3d70be60c0
LazarevSlava <549132w@gmail.com>
2024-01-24 07:01:04 +0200

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { Header } from './components/Header';
-import './App.css';
-import { ToggleThemeContext } from './helpers/ToggleThemeContext';
-import { Search } from './components/Search';
-import { CountrySelector } from './components/CountrySelector';
+import { Header } from "./components/Header";
+import "./App.css";
+import { ToggleThemeContext } from "./helpers/ToggleThemeContext";
+import { Search } from "./components/Search";
+import { CountrySelector } from "./components/CountrySelector";
 
 function App() {
-    const handleSearch = (str) => {
-        console.log(str);
-    };
+  const handleSearch = (str) => {
+    console.log(str);
+  };
 
-    const handleSelect = (region) => {
-        console.log(region);
-    };
+  const handleSelect = (region) => {
+    console.log(region);
+  };
 
-    return (
-        <>
-            <ToggleThemeContext>
-                <Header />
-                <Search onSearch={handleSearch} />
-                <CountrySelector onSelect={handleSelect} />
-            </ToggleThemeContext>
-        </>
-    );
+  return (
+    <>
+      <ToggleThemeContext>
+        <Header />
+        <Search onSearch={handleSearch} />
+        <CountrySelector onSelect={handleSelect} />
+      </ToggleThemeContext>
+    </>
+  );
 }
 export default App;
--- src/components/CountrySelector.jsx
+++ src/components/CountrySelector.jsx
@@ -1,64 +1,48 @@
-import style from './CountrySelector.module.scss';
-import { useTheme } from '../hooks/themeUtils';
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+import style from "./CountrySelector.module.scss";
+import { useTheme } from "../hooks/themeUtils";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 function CountrySelector({ onSelect = Function.prototype }) {
-    const [isActive, setIsActive] = useState(false);
-    const options = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
-    const { theme = 'dark' } = useTheme();
-    const selectClass = `${style.select} ${theme === 'dark' ? ` ${style.light}` : `''`}`;
+  const [isActive, setIsActive] = useState(false);
+  const [selected, setSelected] = useState("Filter by Region");
+  const options = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const { theme = "dark" } = useTheme();
+  const selectClass = ` ${theme === "dark" ? ` ${style.light}` : `''`}`;
 
-    // const handleRegionChange = (event) => {
-    //     const selectedRegion = event.target.value;
-
-    //     onSelect(selectedRegion);
-    // };
-
-    return (
-        // <div className={style.select_field}>
-        //     <select onChange={handleRegionChange} className={selectClass}>
-        //         <option value="" disabled selected>
-        //             Filter by Region
-        //         </option>
-        //         <option value="1">Africa</option>
-        //         <option value="2">America</option>
-        //         <option value="3">Asia</option>
-        //         <option value="4">Europe</option>
-        //         <option value="5">Oceania</option>
-        //     </select>
-        // </div>
-        <div className={style.dropdown}>
-            <div
-                className={style.dropdown_btn}
-                onClick={(e) => {
-                    setIsActive(!isActive);
-                }}
+  return (
+    <div className={style.dropdown}>
+      <button
+        className={`${selectClass} ${style.dropdown_btn}`}
+        onClick={(e) => {
+          setIsActive(!isActive);
+        }}
+      >
+        {selected}
+      </button>
+      {isActive && (
+        <div className={`${selectClass} ${style.dropdown_content}`}>
+          {options.map((option) => (
+            <button
+              key={option}
+              className={`${selectClass} ${style.dropdown_item}`}
+              onClick={(e) => {
+                onSelect(e.target.textContent);
+                setIsActive(false);
+                setSelected(e.target.textContent);
+              }}
             >
-                Filter by Region
-                <span className={`${style.fas} ${style.fa_caret_down}`}></span>
-            </div>
-            {isActive && (
-                <div className={style.dropdown_content}>
-                    {options.map((option) => (
-                        <div
-                            className={style.dropdown_item}
-                            onClick={(e) => {
-                                onSelect(e.target.textContent);
-                                setIsActive(false);
-                            }}
-                        >
-                            {option}
-                        </div>
-                    ))}
-                </div>
-            )}
+              {option}
+            </button>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 CountrySelector.propTypes = {
-    onSelect: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
 
 export { CountrySelector };
