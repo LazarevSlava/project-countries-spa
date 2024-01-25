0334ea6cd58460df9df6ad8045e8c8d0fe4b0bfe
LazarevSlava <549132w@gmail.com>
2024-01-25 16:46:45 +0200

    fixed errors

--- src/components/CountrySelector.jsx
+++ src/components/CountrySelector.jsx
@@ -2,14 +2,38 @@ import style from "./CountrySelector.module.scss";
 import { useTheme } from "../hooks/themeUtils";
 import PropTypes from "prop-types";
 import { useState } from "react";
-
+import IconArrowDown from "../assets/images/arrow-down-svgrepo-com.svg?react";
+import IconClose from "../assets/images/close_FILL0_wght400_GRAD0_opsz24.svg?react";
 function CountrySelector({ onSelect = Function.prototype }) {
   const [isActive, setIsActive] = useState(false);
-  const [selected, setSelected] = useState("Filter by Region");
+  const [selected, setSelected] = useState("");
   const options = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const { theme = "dark" } = useTheme();
   const lightThemeClass = theme === "dark" ? style.light : "";
   const selectClass = ` ${lightThemeClass}`;
+  const themeIconArrow =
+    theme === "dark" ? (
+      <IconArrowDown className={style.icon} />
+    ) : (
+      <IconArrowDown className={style.icon} fill="white" />
+    );
+  const themeIconClose =
+    theme === "dark" ? (
+      <IconClose
+        className={style.icon}
+        onClick={() => {
+          setSelected("");
+        }}
+      />
+    ) : (
+      <IconClose
+        className={style.icon}
+        onClick={() => {
+          setSelected("");
+        }}
+        fill="white"
+      />
+    );
 
   return (
     <div className={style.dropdown}>
@@ -19,7 +43,8 @@ function CountrySelector({ onSelect = Function.prototype }) {
           setIsActive(!isActive);
         }}
       >
-        {selected}
+        {!selected ? "Filter by Region" : selected}
+        {!selected ? themeIconArrow : themeIconClose}
       </button>
       {isActive && (
         <div className={`${selectClass} ${style.dropdown_content}`}>
