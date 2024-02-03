2fab4474c88c62121563fdfbd14170108bffe5b1
Mikhail Nepomniashchii <michey85@gmail.com>
2024-02-03 22:48:51 +0100

    Refactor CountrySelector component and fix theme issue

--- src/components/CountrySelector.jsx
+++ src/components/CountrySelector.jsx
@@ -1,22 +1,25 @@
-import style from "./CountrySelector.module.scss";
-import { useTheme } from "../hooks/themeUtils";
 import PropTypes from "prop-types";
 import { useState } from "react";
+import style from "./CountrySelector.module.scss";
+import { useTheme } from "../hooks/themeUtils";
 import IconArrowDown from "../assets/images/arrow-down-svgrepo-com.svg?react";
 import IconClose from "../assets/images/close_FILL0_wght400_GRAD0_opsz24.svg?react";
+
+const options = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
 function CountrySelector({ onSelect = Function.prototype }) {
   const [isActive, setIsActive] = useState(false);
   const [selected, setSelected] = useState("");
-  const options = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const { theme = "dark" } = useTheme();
-  const lightThemeClass = theme === "dark" ? style.light : "";
-  const selectClass = ` ${lightThemeClass}`;
+  const selectClass = theme === "dark" ? style.light : "";
+
   const themeIconArrow = (
     <IconArrowDown
       className={style.icon}
       fill={theme === "light" ? "white" : "black"}
     />
   );
+  
   const themeIconClose = (
     <IconClose
       className={style.icon}
@@ -45,10 +48,10 @@ function CountrySelector({ onSelect = Function.prototype }) {
             <button
               key={option}
               className={`${selectClass} ${style.dropdown_item}`}
-              onClick={(e) => {
-                onSelect(e.target.textContent);
+              onClick={() => {
+                onSelect(option);
                 setIsActive(false);
-                setSelected(e.target.textContent);
+                setSelected(option);
               }}
             >
               {option}
