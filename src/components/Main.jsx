030afc923413e5d2e6798246975d8af4a0345ef5
LazarevSlava <549132w@gmail.com>
2024-03-05 11:20:52 +0100

    useMemo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,77 +1,60 @@
 import style from './Main.module.scss';
 import { useTheme } from '../hooks/themeUtils';
-
 import { Search } from './Search';
 import { CountrySelector } from './CountrySelector';
 import { API_URL } from '../config.js';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Preloader from './preloader/Preloader';
 import { CountriesList } from './CountriesList';
 
 function Main() {
   const [isLoading, setLoading] = useState(false);
-  const [countryName, setCountryName] = useState('');
-  const [regionName, setRegionName] = useState('');
   const [countries, setCountries] = useState([]);
-  const [countriesCatalog, setCountriesCatalog] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('');
   const { theme = 'dark' } = useTheme();
 
   const containerClassnames = `${style.main} ${theme === 'dark' ? style.light : ''}`;
 
   const handleSelect = (region) => {
-    setRegionName(region);
-    if (countryName === '') {
-      setCountriesCatalog(
-        countries.filter((item) => item.region.toLowerCase().includes(region.toLowerCase())),
-      );
-    } else {
-      let countrySelect = [];
-
-      countrySelect = countries.filter((item) =>
-        item.region.toLowerCase().includes(region.toLowerCase()),
-      );
-
-      setCountriesCatalog(
-        countrySelect.filter((item) =>
-          item.name.common.toLowerCase().includes(countryName.toLowerCase()),
-        ),
-      );
-    }
+    setSelectedRegion(region);
   };
 
   const handleEmptySelect = () => {
-    setCountriesCatalog(
-      countries.filter((item) =>
-        item.name.common.toLowerCase().includes(countryName.toLowerCase()),
-      ),
-    );
+    setSelectedRegion('');
   };
 
   const handleSearch = (str) => {
-    setCountryName(str);
-    if (regionName === '') {
-      setCountriesCatalog(
-        countries.filter((item) => item.name.common.toLowerCase().includes(str.toLowerCase())),
-      );
-    } else {
-      let regionSelect = [];
+    setSearchInput(str);
+  };
 
-      regionSelect = countries.filter((item) =>
-        item.region.toLowerCase().includes(regionName.toLowerCase()),
-      );
+  const filteredCountries = useMemo(() => {
+    if (searchInput === '' && selectedRegion === '') {
+      return countries;
+    }
 
-      setCountriesCatalog(
-        regionSelect.filter((item) => item.name.common.toLowerCase().includes(str.toLowerCase())),
+    let filtered = countries;
+
+    if (selectedRegion !== '') {
+      filtered = filtered.filter((item) =>
+        item.region.toLowerCase().includes(selectedRegion.toLowerCase()),
       );
     }
-  };
+
+    if (searchInput !== '') {
+      filtered = filtered.filter((item) =>
+        item.name.common.toLowerCase().includes(searchInput.toLowerCase()),
+      );
+    }
+
+    return filtered;
+  }, [countries, searchInput, selectedRegion]);
 
   useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
       .then((data) => {
         setCountries(data);
-        setCountriesCatalog(data);
       })
       .then(setLoading(true))
       .catch((err) => {
@@ -85,7 +68,7 @@ function Main() {
       <Search onSearch={handleSearch} />
       <CountrySelector onSelect={handleSelect} handleEmptySelect={handleEmptySelect} />
       <div className={style.cardContainer}>
-        {isLoading ? <CountriesList countries={countriesCatalog} /> : <Preloader />}
+        {isLoading ? <CountriesList countries={filteredCountries} /> : <Preloader />}
       </div>
     </div>
   );
